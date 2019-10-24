d296dcc69f0ca9dd6b892bec25d950ab6c0aa319
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2019-10-24 15:54:07 +0200

    Added custom React page `backendtest` for testing purposes. Fixed `public.html` being overwritten by Copy-Plugin.

---
+++ WebService/src/main/app/src/backendtest.js
@@ -0,0 +1,21 @@
+const worker = new Worker('worker.js');
+
+function sayHI2() {
+    worker.postMessage({'cmd': 'start', 'msg': 'Hi'});
+}
+
+function unknownCmd2() {
+    worker.postMessage({'cmd': 'foobard', 'msg': '???'});
+}
+
+function stop2() {
+    worker.postMessage({'cmd': 'stop', 'msg': 'Bye'});
+}
+
+worker.addEventListener('message', function(e) {
+    document.getElementById('result2').textContent = e.data;
+}, false);
+
+document.getElementById("hi2").addEventListener('click', sayHI2);
+document.getElementById("unknown2").addEventListener('click', unknownCmd2);
+document.getElementById("stop2").addEventListener('click', stop2);
--- WebService/src/main/app/webpack.config.js
+++ WebService/src/main/app/webpack.config.js
@@ -1,9 +1,15 @@
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const CopyWebPackPlugin = require("copy-webpack-plugin");
+const HtmlWebpackExcludeAssetsPlugin = require('html-webpack-exclude-assets-plugin');
+
 const path = require('path');
 
 // Configure React-App
 const appConfig = {
+  entry: {
+    'index': './src/index.js',
+    'backendtest': './src/backendtest.js'
+  },
   module: {
     rules: [
       {
@@ -28,20 +34,24 @@ const appConfig = {
       {
         test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
         loader: 'url-loader?limit=100000'
-      },
-      {
-        test: /\.wasm$/,
-        loader: ['wasm-loader']
-      },
+      }
     ]
   },
   plugins: [
     new HtmlWebPackPlugin({
       template: "./public/index.html",
-      filename: "./index.html"
+      filename: "./index.html",
+      excludeAssets: [/backendtest.*.js/]
     }),
+    new HtmlWebpackExcludeAssetsPlugin(),
+    new HtmlWebPackPlugin({
+      template: "./public/backendtest.html",
+      filename: "./backendtest.html",
+      excludeAssets: [/index.*.js/]
+    }),
+    new HtmlWebpackExcludeAssetsPlugin(),
     new CopyWebPackPlugin([
-      { from: "public" }
+      { from: "public", ignore: ["index.html"] }
     ])
   ],
   output: {
