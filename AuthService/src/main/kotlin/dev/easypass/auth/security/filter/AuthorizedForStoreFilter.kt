abd2f005878c03bf41a4f967725d3979974f842b
mwustinger-tgm <Wustinger.Martin@gmail.com>
2020-01-27 15:05:05 +0100

    Filer added, User can only acces his own database

--- AuthService/src/main/kotlin/dev/easypass/auth/security/filter/AuthorizedForStoreFilter.kt
+++ AuthService/src/main/kotlin/dev/easypass/auth/security/filter/AuthorizedForStoreFilter.kt
@@ -2,11 +2,13 @@ package dev.easypass.auth.security.filter
 
 import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.web.access.intercept.FilterSecurityInterceptor
 import org.springframework.security.web.authentication.WebAuthenticationDetails
 import org.springframework.security.web.util.matcher.RequestMatcher
 import org.springframework.stereotype.Component
 import org.springframework.stereotype.Service
 import org.springframework.web.filter.GenericFilterBean
+import org.springframework.web.filter.OncePerRequestFilter
 import java.util.*
 import javax.servlet.FilterChain
 import javax.servlet.ServletRequest
@@ -15,15 +17,17 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 @Component
-class AuthorizedForStoreFilter(private val properties: Properties): GenericFilterBean() {
-    override fun doFilter(request: ServletRequest?, response: ServletResponse?, chain: FilterChain?) {
-        val url = (request as HttpServletRequest).requestURL.toString()
+class AuthorizedForStoreFilter(private val properties: Properties): OncePerRequestFilter() {
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, filterChain: FilterChain) {
+        val url = request.requestURL.toString()
         val authorities = AuthorityUtils.authorityListToSet(SecurityContextHolder.getContext().authentication.authorities)
         val store = ":${properties.getProperty("server.port")}/store/"
-        if (url.contains(store)){
-            val uname = url.substringAfter(store).split("/")[0]
-            (response as HttpServletResponse).sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
+        val hash = url.substringAfter(store).split("/")[0]
+        println(url)
+        if (url.contains(store) and authorities.isNotEmpty()) {
+            if (!(authorities.contains("USER_$hash") or authorities.contains("GROUP_$hash") or authorities.contains("ADMIN_$hash")))
+                response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Unauthorized")
         }
-        chain?.doFilter(request, response)
+        filterChain.doFilter(request, response)
     }
 }
