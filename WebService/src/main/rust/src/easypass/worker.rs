5e10103a0a5bc6f057db603abc9d259e1a0bb0e0
Kacper Urbaniec <at.kacper.urbaniec@gmail.com>
2020-02-12 21:04:53 +0100

    Worker struct uses now RefCells instead of Mutexes

--- WebService/src/main/rust/src/easypass/worker.rs
+++ WebService/src/main/rust/src/easypass/worker.rs
@@ -43,6 +43,8 @@ extern {
     fn sleep(timeout: u64);
 }
 
+
+/// Manages databases and performs CRUD-operations.
 pub struct Worker {
     private: Connection,
     service_status: RefCell<String>,
@@ -52,6 +54,8 @@ pub struct Worker {
     password_clear: RefCell<HashMap<u16, Timeout>>
 }
 
+/// Holds one logical databases with references to the local and remote one.
+/// Also contains the changes-feed and sync-handler and their used closures.
 pub struct Connection {
     local_db: PouchDB,
     remote_db: RefCell<Option<PouchDB>>,
@@ -59,12 +63,14 @@ pub struct Connection {
     sync: RefCell<Option<Sync>>
 }
 
+/// Holds the changes-feed of the database and its used closures.
 pub struct Changes {
     changes_feed: ChangesFeed,
     change: RefCell<Option<Closure<dyn FnMut(JsValue)>>>,
     error: RefCell<Option<Closure<dyn FnMut(JsValue)>>>
 }
 
+/// Holds the sync-handler of the database and its used closures.
 pub struct Sync {
     sync_handler: SyncHandler,
     change: Closure<dyn FnMut(JsValue)>,
@@ -88,17 +94,16 @@ impl Worker {
         };
         // Setup changes feed for database of private entries
         let changes_feed = local_db.changes();
-
+        // Closures for the changes-feed will be initialized later properly
         let change = RefCell::new(None);
         let error = RefCell::new(None);
-
         // Create struct that holds everything relevant to changes
         let changes = Changes {
             changes_feed,
             change,
             error
         };
-
+        // Create temporary sync struct
         let sync = RefCell::new(None);
         // Create struct that holds everything relevant to database of private entries
         let private = Connection {
@@ -107,7 +112,8 @@ impl Worker {
             changes,
             sync
         };
-        // Create worker
+        // Create worker with reference counting to enable its usage
+        // in multiple parts in the application
         let worker = Rc::new(Worker {
             private,
             service_status: RefCell::new(String::from("online")),
@@ -116,29 +122,28 @@ impl Worker {
             password_cache: RefCell::new(HashMap::new()),
             password_clear: RefCell::new(HashMap::new()),
         });
-
-        // Define functionality on database update
-        // Send all documents to ui on change
+        // With the reference to the Worker the functionality
+        // for database updates can be defined
         let worker_moved_change = worker.clone();
         let change = Closure::new(move |val: JsValue| {
-            let worker = worker_moved_change;
+            let worker = &worker_moved_change;
             log("We have a change!");
+            // Send all documents to ui on change
             Worker::all_docs_without_passwords(&worker.private.local_db);
         });
-        // Define functionality on database error
+        // With the reference to the Worker the functionality
+        // for database errors can be defined
         let worker_moved_error = worker.clone();
         let error = Closure::new(move |val: JsValue| {
-            let worker = worker_moved_error;
+            let worker = &worker_moved_error;
             log("EEEEEEEEEEEERRRRRRRRROOOOOOOOOOORRRRRRRRR!");
         });
-
-        // Bind on change functions
+        // On change functions need to be bound to the changes feed
         worker.private.changes.changes_feed.on_change(&change);
         worker.private.changes.changes_feed.on_error(&error);
-
-        worker.private.changes.change.replace(change);
-        worker.private.changes.error.replace(error);
-
+        worker.private.changes.change.replace(Some(change));
+        worker.private.changes.error.replace(Some(error));
+        // Return referenced counted worker
         worker
     }
 
@@ -203,7 +208,6 @@ impl Worker {
 
     pub fn save_password(self: Rc<Worker>, data: JsValue) -> Promise {
         // TODO Worker Decrypt Password
-
         future_to_promise(async move {
             let result
                 = JsFuture::from(self.private.local_db.post(&data)).await;
@@ -213,7 +217,6 @@ impl Worker {
     }
 
     pub fn update_password(self: Rc<Worker>, data: JsValue) -> Promise {
-
         future_to_promise(async move {
             let result
                 = JsFuture::from(self.private.local_db.put(&data)).await;
@@ -223,19 +226,14 @@ impl Worker {
     }
 
     pub fn delete_password(self: Rc<Worker>, data: JsValue) -> Promise {
-        // Bind private database and cache for deleted password entries
-
-        let cache = Arc::clone(&self.password_cache);
-        // Clear that stores deletion routines is needed two times (in- and outside of closure)
-        let mut clear = Arc::clone(&self.password_clear);
-        let mut this_clear = Arc::clone(&self.password_clear);
-        // Parse received password entry
-        let mut entry: Value = data.into_serde().unwrap();
-        // Move values to promise, allows usage of async
         future_to_promise(async move {
-
+            // Bind private database and cache for deleted password entries
             let private_db = &self.private.local_db;
-
+            let cache = &self.password_cache;
+            // Clear is a hashmap which stores deletion routines (closures)
+            let mut clear = self.password_clear.borrow_mut();
+            // Parse received password entry
+            let mut entry: Value = data.into_serde().unwrap();
             // Stores id of password entry that is used to get fully and later for deletion
             let id = String::from(entry["_id"].as_str().unwrap());
             // Get revision which is needed for deletion
@@ -245,13 +243,10 @@ impl Worker {
                 // Get full entry for backup
                 let backup_raw
                     = JsFuture::from(private_db.get(&id)).await.unwrap();
-                // Lock and get full backup data
-                let mut cache
-                    = cache.lock().unwrap_or_else(PoisonError::into_inner);
                 let recovery = RecoverPassword::new(&backup_raw);
                 log(&format!("RECOVERY: {:?}", &recovery));
                 // Add full password entry to password cache
-                cache.insert(
+                cache.borrow_mut().insert(
                     String::from(&id),
                     recovery
                 );
@@ -261,11 +256,7 @@ impl Worker {
                 = JsFuture::from(private_db.remove(&JsValue::from(&id), &rev));
             let result = action.await;
             Worker::build_and_post_message("deletePassword", result.unwrap());
-
             // Delete password entry after a timeout
-            // Get hashmap which stores deletion routines (closures)
-            let mut clear
-                = clear.lock().unwrap_or_else(PoisonError::into_inner);
             // Generate id for new routine
             let mut rng = rand::thread_rng();
             let mut closure_id: u16 = rng.gen_range(0, 200);
@@ -274,16 +265,18 @@ impl Worker {
             }
             // Clone it for later use in closure
             let this_closure_id = closure_id.clone();
+            // Clone state to move it to closure
+            let worker = self.clone();
             // Generate closure and add to hashmap
             clear.insert(
                 closure_id,
                 Timeout::new(move || {
                     // Get hashmap that stores all routines
                     let mut clear
-                        = this_clear.lock().unwrap_or_else(PoisonError::into_inner);
-                    // Lock and get backup data
+                        = worker.password_clear.borrow_mut();
+                    // Get backup data
                     let mut cache
-                        = cache.lock().unwrap_or_else(PoisonError::into_inner);
+                        = worker.password_cache.borrow_mut();
                     log(&format!("B. Cache {:?}", &cache));
                     // Perform deletion
                     if cache.contains_key(&id) {
@@ -299,19 +292,13 @@ impl Worker {
     }
 
     pub fn undo_delete_password(self: Rc<Worker>, data: JsValue) -> Promise {
-        // Bind private database and cache for deleted password entries
-
-        let cache = Arc::clone(&self.password_cache);
-        // Parse received password entry
-        let mut entry: Value = data.into_serde().unwrap();
-        // Move values to promise, allows usage of async
         future_to_promise(async move {
-
+            // Bind private database
             let private_db = &self.private.local_db;
-
-            // Lock and get full backup data
-            let mut cache
-                = cache.lock().unwrap_or_else(PoisonError::into_inner);
+            // Get full backup data
+            let mut cache = self.password_cache.borrow_mut();
+            // Parse received password entry
+            let mut entry: Value = data.into_serde().unwrap();
             // Get id of entry to recover
             let id = String::from(entry["_id"].as_str().unwrap());
             // Check if cache still contains this password entry
@@ -343,7 +330,6 @@ impl Worker {
 
     pub fn save_category(self: Rc<Worker>, data: JsValue) -> Promise {
         // TODO Worker Decrypt Password
-
         future_to_promise(async move {
             let result
                 = JsFuture::from(self.private.local_db.post(&data)).await;
@@ -353,7 +339,6 @@ impl Worker {
     }
 
     pub fn update_category(self: Rc<Worker>, data: JsValue) -> Promise {
-
         future_to_promise(async move {
             let result
                 = JsFuture::from(self.private.local_db.put(&data)).await;
@@ -363,29 +348,22 @@ impl Worker {
     }
 
     pub fn delete_categories(self: Rc<Worker>, categories: JsValue) -> Promise {
-        // Convert JsValue to Array, that its truly is
-        let categories = Array::from(&categories);
-        // Bind private database and cache for deleted password entries
-
-        let mut cache = Arc::clone(&self.category_cache);
-        // Clear that stores deletion routines is needed two times (in- and outside of closure)
-        let mut clear = Arc::clone(&self.category_clear);
-        let mut this_clear = Arc::clone(&self.category_clear);
-        // Parse received categories as vec
-        let mut categories: Vec<Value> = categories.into_serde().unwrap();
-        // This variable will be used to store the categories that need to be deleted
-        let query = Array::new_with_length(categories.len() as u32);
-        // Move values to promise, allows usage of async
         future_to_promise(async move {
-
+            // Convert JsValue to Array, that its truly is
+            let categories = Array::from(&categories);
+            // Bind private database and cache for deleted password entries
             let private_db = &self.private.local_db;
-
+            let mut cache = self.category_cache.borrow_mut();
+            // Clear is a hashmap which stores deletion routines (closures)
+            let mut clear = self.category_clear.borrow_mut();
+            // Parse received categories as vec
+            let mut categories: Vec<Value> = categories.into_serde().unwrap();
+            // This variable will be used to store the categories that need to be deleted
+            let query = Array::new_with_length(categories.len() as u32);
             // Stores received category ids that need to be deleted later on
             let mut clear_ids = Vec::new();
             // Enumerate over received categories
             for (i, cat) in categories.iter_mut().enumerate() {
-                let mut cache
-                    = cache.lock().unwrap_or_else(PoisonError::into_inner);
                 // Add "_deleted" tag for later bulk operation that deletes this category
                 cat["_deleted"] = Bool(true);
                 // Get full category entry, is needed for proper undo of deletion
@@ -428,11 +406,7 @@ impl Worker {
             let action = JsFuture::from(private_db.bulk_docs(&query));
             let result = action.await;
             Worker::build_and_post_message("deleteCategories", result.unwrap());
-
             // Delete category cache after a timeout
-            // Get hashmap which stores deletion routines (closures)
-            let mut clear
-                = clear.lock().unwrap_or_else(PoisonError::into_inner);
             // Generate id for new routine
             let mut rng = rand::thread_rng();
             let mut closure_id: u16 = rng.gen_range(0, 200);
@@ -441,19 +415,20 @@ impl Worker {
             }
             // Clone it for later use in closure
             let this_closure_id = closure_id.clone();
+            // Clone state to move it to closure
+            let worker = self.clone();
             // Generate closure and add to hashmap
             clear.insert(
                 closure_id,
                 Timeout::new(move || {
                     // Get hashmap that stores all routines
-                    let mut clear
-                        = this_clear.lock().unwrap_or_else(PoisonError::into_inner);
+                    let mut clear = worker.category_clear.borrow_mut();
                     // Fixes: "cannot move out of `this_clear_ids`, a captured variable in an
                     // `FnMut` closure" error
                     let mut clear_ids = clear_ids.to_vec();
                     // Lock and get backup data
-                    let mut cache
-                        = cache.lock().unwrap_or_else(PoisonError::into_inner);
+                    let mut cache = worker.category_cache.borrow_mut();
+                    log(&format!("C. Cache {:?}", &cache));
                     // Perform deletion
                     for category in clear_ids.into_iter() {
                         if cache.contains_key(&category) {
@@ -462,6 +437,7 @@ impl Worker {
                     }
                     // Remove this routine
                     clear.remove(&this_closure_id);
+                    log(&format!("C. Cache-len {:?}", &cache.len()));
                 }, 7500));
             Ok(JsValue::from(true))
         })
@@ -469,23 +445,16 @@ impl Worker {
 
 
     pub fn undo_delete_categories(self: Rc<Worker>, categories: JsValue) -> Promise {
-        // Convert JsValue to Array, that its truly is
-        let categories = Array::from(&categories);
-        // Bind private database and cache for deleted password entries
-
-        let cache = Arc::clone(&self.category_cache);
-        // Convert received category data to vec
-        let categories: Vec<Value> = categories.into_serde().unwrap();
-        // Move values to promise, allows usage of async
         future_to_promise(async move {
-
+            // Convert JsValue to Array, that its truly is
+            let categories = Array::from(&categories);
+            // Bind private database and cache for deleted password entries
             let private_db = &self.private.local_db;
-
+            let mut cache = self.category_cache.borrow_mut();
+            // Convert received category data to vec
+            let categories: Vec<Value> = categories.into_serde().unwrap();
             // Iterate over received categories that should be recovered
             for category in categories {
-                // Lock and get full backup data
-                let mut cache
-                    = cache.lock().unwrap_or_else(PoisonError::into_inner);
                 // Get id of entry to recover
                 let cat_id = String::from(category["_id"].as_str().unwrap());
                 // Check if cache still contains this category entry
@@ -520,18 +489,18 @@ impl Worker {
     }
 
     pub fn get_password(self: Rc<Worker>, cmd: String, data: JsValue) -> Promise {
-        // Bind private database
-        let private_db = &self.private.local_db;
-        // Parse data to make it readable from Rust
-        let data_parsed = data.into_serde::<Value>().unwrap();
-        // Search for received entry
-        let action = JsFuture::from(private_db.find(&JsValue::from_serde(&json!({
-            "selector": {
-                "_id": data_parsed["_id"],
-                "_rev": data_parsed["_rev"],
-            }
-        })).unwrap()));
         future_to_promise(async move {
+            // Bind private database
+            let private_db = &self.private.local_db;
+            // Parse data to make it readable from Rust
+            let data_parsed = data.into_serde::<Value>().unwrap();
+            // Search for received entry
+            let action = JsFuture::from(private_db.find(&JsValue::from_serde(&json!({
+                "selector": {
+                    "_id": data_parsed["_id"],
+                    "_rev": data_parsed["_rev"],
+                }
+            })).unwrap()));
             // Parse received entry
             let result_raw = action.await.unwrap();
             let result_raw = result_raw.into_serde::<Value>().unwrap();
@@ -589,7 +558,7 @@ impl Worker {
     }
 
     // TODO refactor?
-    pub fn get_service_status(self: Rc<Worker>) -> Arc<Mutex<String>> {
+    pub fn get_service_status(self: Rc<Worker>) -> RefCell<String> {
         self.service_status.clone()
     }
 }
