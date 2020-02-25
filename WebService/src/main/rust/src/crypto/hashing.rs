296dd00fd06e1eef8dceccb435b09c10cefdb0af
Moritz Welsch <mwelsch@student.tgm.ac.at>
2020-02-25 11:33:53 +0100

    Added hashing functions

--- WebService/src/main/rust/src/crypto/hashing.rs
+++ WebService/src/main/rust/src/crypto/hashing.rs
@@ -1,20 +1,18 @@
 extern crate argon2;
 extern crate base64;
 
-pub fn hash(string: &str, salt: String) -> String{
+pub fn hash_argon(string: &str, salt: String) -> String{
     let salz = salt.get_byte;
     let mut config = Config::default();
-    config.hash_length = 32;
+    config.hash_length = 64;
     let mut hash = argon2::hash_encoded(string, salt, &config).unwrap();
     return hash;
 }
-
-
-
-pub fn getKeyFromPassword(password: &str, salt: String) -> &[u8] {
-    let salz = salt.get_byte;
-    let mut config = Config::default();
-    config.hash_length = 32;
-    let mut hash = argon2::hash_raw(string, salt, &config).unwrap();
-    return hash;
+pub fn hash_sha3_256(password: &String) -> Vec<u8> {
+    let mut hasher = Sha3_256::new(); 
+    hasher.input(password.as_bytes());
+    let key_hash = hasher.result();
+    let key = key_hash.to_vec();
+    return key;
 }
+
